02aa1a0fce2cf131c348df61a184b8b28b7bb4e0
Eero Olli <eero.olli@gmail.com>
2023-04-28 17:02:29 +0200

    better comments, deleted unneeded files

--- check_odiers_website.py
+++ check_odiers_website.py
@@ -14,14 +14,13 @@ from configparser import ConfigParser, ExtendedInterpolation
 config = ConfigParser(interpolation=ExtendedInterpolation())
 config.read('email.ini')
 
-
+#### Define ==========================
 
 # Define the URL and other parameters
 url = "https://danielodier.com/en/seminars-list"
 
-seminars_csv = "odier_seminars.csv"
+seminars_csv = "odier_seminars.csv"  # This is where the previous info is saved.
 
-# Set the sender and receiver email addresses
 sender_email = "reminder@posturepower.de"
 receiver_emails = ["eero.olli@gmail.com", "info@posturepower.de"]
 
@@ -31,6 +30,7 @@ smtp_port=465
 sender_email=config['EMAIL']['username']
 
 
+#### ==============================
 try:
     # resp = request.urlopen(url)
     resp = requests.get(url)
@@ -44,18 +44,20 @@ finally:
         print("Content of webpage is downloaded.")
 
 
+events = body.find_all("a", class_="btn btn-prenota")    # this class contain the seminars
 
-
-events = body.find_all("a", class_="btn btn-prenota")
-
+#### Saved events ================================
+# Extract the highest number from the saved seminars
 with open(seminars_csv, mode='r') as file:
     reader = csv.DictReader(file)
     saved_seminars = [row for row in reader]
 
-# Extract the highest number from the saved seminars
 saved_seminars_numbers = [int(seminar['number']) for seminar in saved_seminars]
-max_saved_number = max(saved_seminars_numbers)
+max_saved_number = max(saved_seminars_numbers)  # The assumption is that they number seminars in the order.
+# I can imagine that there might be errors but I will not deal with them.
 
+
+#### New events ================================
 
 # Create a list of dictionaries to store the online events
 events_online = []
@@ -72,7 +74,9 @@ for event in events:
 online_seminars_numbers = [event['number'] for event in events_online]
 max_online_number = max(online_seminars_numbers)
 
-# Compare the highest numbers and print the result
+
+#### Compare the highest numbers and send email ============= 
+
 if max_saved_number >= max_online_number:
     print("No new seminars")
 
@@ -98,7 +102,7 @@ Eero
         for receiver_email in receiver_emails:
             server.sendmail(sender_email, receiver_email, message)
 
-    # Replace the saved csv file with the updated online events
+    # Replace the saved csv file with the updated online events if needed
     with open(seminars_csv, mode='w') as file:
         fieldnames = ['number', 'name']
         writer = csv.DictWriter(file, fieldnames=fieldnames)
