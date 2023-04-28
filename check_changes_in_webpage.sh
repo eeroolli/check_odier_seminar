eb8bddfb0e69482020627f533c8db1331486bf25
Eero Olli <eero.olli@gmail.com>
2023-04-28 16:19:57 +0200

    cleaning up

--- check_changes_in_webpage.sh
+++ check_changes_in_webpage.sh
@@ -6,11 +6,8 @@ source /path/to/venv/bin/activate ds
 # Change to the directory containing the Python script
 cd /home/eero_ds/dataanalyst/check_odier_seminar/
 
-# Run the Python continuously script in the background
+# Run the Python continuously script in the background. 
 # nohup python check_odiers_website.py > /dev/null 2>&1 &
 
-# Run the Python continuously script in the background
-# nohup python check_odiers_website.py > /dev/null 2>&1 &
-
-# Run the Python script with a cron job
+# Run the Python script with a cron job takes less resources.
 python check_odiers_website.py
